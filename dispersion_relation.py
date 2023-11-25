0fb58999f9435d9ad316a51b43cc13e7b61eab49
Kishore <kishoreg@iucaa.in>
2023-11-25 16:49:10 +0530

    dispersion_relation.get_mode_eigenfunction: new function
    To find the z-dependence of the eigenfunction of a mode.
    Handling of near-node regions is still not satisfactory.

--- dispersion_relation.py
+++ dispersion_relation.py
@@ -345,6 +345,30 @@ def fit_mode(dr, k_tilde, z, om_tilde_min, om_tilde_max, poly_order, n_lorentz):
 	
 	return model
 
+def get_mode_eigenfunction(dr, omega_0, k_tilde, z_list, om_tilde_min, om_tilde_max):
+	"""
+	Use fit_mode to get the z-dependent eigenfunction of the mode whose frequency (omega_tilde) is close to omega_0 at k_tilde.
+	"""
+	if not om_tilde_min < omega_0 < om_tilde_max:
+		raise ValueError("Cannot fit mode that is outside search band.")
+	
+	P_list = []
+	for z in z_list:
+		fit = fit_mode(dr, k_tilde, z, om_tilde_min, om_tilde_max, poly_order=1, n_lorentz=1)
+		_, params_lorentz = fit.unpack_params(fit.popt)
+		
+		omt_near_target, _ = dr.get_data_at_kz(k_tilde, z, omega_tilde_min=om_tilde_min, omega_tilde_max=om_tilde_max)
+		
+		domega = np.abs(omega_0 - params_lorentz[:,1]) #how far each detected mode is from omega_0
+		imode = np.argmin(domega)
+		print(f"{z = :.2f}, dist_from mode = {domega[imode]:.2e}, omega spacing = {omt_near_target[1] - omt_near_target[0]:.2e}") #debug
+		
+		mode = fit.lorentzian(omt_near_target, *params_lorentz[imode])
+		mode_mass = np.trapz(mode, omt_near_target)
+		P_list.append(mode_mass)
+	
+	return np.array(P_list)
+
 def smooth(data, n):
 	"""
 	data: numpy array
