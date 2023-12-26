d78f0842c90313fe711f646f8d11c921af64ef68
Kishore <kishoreg@iucaa.in>
2023-12-26 15:39:51 +0530

    Change default choice of data scaling.

--- default.py
+++ default.py
@@ -4,8 +4,8 @@ Only meant for quick interactive use; may change without notice.
 
 from . import read
 
-class dr_yaver(read.m_scl_HP, read.m_dscl_rdbyD2, read.dr_yaver_base):
+class dr_yaver(read.m_dscl_rdbyurmsmax, read.m_scl_HP, read.dr_yaver_base):
 	pass
 
-class dr_dvar(read.m_dscl_rdbyD2, read.m_scl_HP, read.dr_dvar_base):
+class dr_dvar(read.m_dscl_rdbyurmsmax, read.m_scl_HP, read.dr_dvar_base):
 	pass
