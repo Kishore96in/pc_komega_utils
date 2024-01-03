27532a8c5b5802e19aa6390a9c123fddcc28f9d0
Kishore <kishoreg@iucaa.in>
2024-01-03 10:43:56 +0530

    dr_base.set_t_Range: correctly handle t_max = None
    Sets t_max = ts.t[-1] in that case.

--- read.py
+++ read.py
@@ -234,6 +234,9 @@ class dr_base(metaclass=abc.ABCMeta):
 		return arr[it_min:it_max]
 	
 	def set_t_range(self, t_min, t_max=None):
+		if t_max is None:
+			t_max = self.ts.t[-1]
+		
 		self._t_min = t_min
 		self._t_max = t_max
 		self.do_ft()
