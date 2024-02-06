7403a1bc4dae8a3e2ec96fc99d97d3704bbb07a7
Kishore <kishoreg@iucaa.in>
2024-02-06 15:29:25 +0530

    m_dscl_dbycz0: new data scaling mixin

--- read.py
+++ read.py
@@ -679,6 +679,23 @@ class m_dscl_rdbycz0():
 		data = np.moveaxis(data, -1, self.data_axes['omega_tilde'])
 		return data
 
+class m_dscl_dbycz0():
+	@property
+	def cbar_label_default(self):
+		return  r"$\left| \widetilde{{P}} \right|$"
+	
+	def scale_data(self, data):
+		gamma = self.param.gamma
+		cp = self.param.cp
+		TTmz = np.average(self.slice_time(self.av_xy.t, self.av_xy.xy.TTmz), axis=0)
+		c2mz = (gamma-1)*cp*TTmz
+		
+		#Get the sound speed at z=0
+		iz0 = np.argmin(np.abs(self.z - 0))
+		c = np.sqrt(c2mz[iz0])
+		
+		return np.abs(data/c)
+
 class m_scl_SBC15(m_dscl_rdbyD2):
 	"""
 	Use the length and frequency scales defined by Singh et al, 2015.
