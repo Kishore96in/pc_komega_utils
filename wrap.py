9aab9cf4cfb5e0b91c219a45355ba4428b2cc6b8
Kishore <kishoreg@iucaa.in>
2024-03-14 12:40:34 +0530

    drs_holder: enhancement: use the realizations to calculate the mean and error of the data
    Now, drs_holder is a dr_base instance.

--- wrap.py
+++ wrap.py
@@ -230,18 +230,26 @@ class drs_holder():
 	"""
 	Holds dispersion relations calculated from multiple realizations of the same simulation setup. The attribute 'realizations' allows to access the individual realizations.
 	
+	This also calculates the mean and error of the data using the given multiple realizations (you can use it just as you would a dr_base instance)
+	
 	Arguments:
-		disp_rel: read.dr_base instance
+		dr_type: read.dr_base instance
 		simdirs: list of strings containing the paths to the different realizations
-		kwargs: dict. Keyword arguments to be passed to disp_rel
 		cachedir: string. Path to folder to use to cache results (uses the `joblib` package). Defaults to None (no caching).
+		All other keyword arguments are passed to dr_type.
 	"""
+	_suffix = "multiwrap"
+	
+	def __new__(cls, dr_type, simdirs, **kwargs):
+		newcls = type(f"{dr_type.__name__}_{cls._suffix}", (cls, dr_type,) , {})
+		obj = object.__new__(newcls)
+		return obj
+	
 	def __init__(
 		self,
-		disp_rel,
+		dr_type,
 		simdirs,
-		kwargs = None,
-		cachedir = None,
+		**kwargs
 		):
 		if kwargs is None:
 			kwargs = {}
@@ -249,18 +257,20 @@ class drs_holder():
 		if len(simdirs) < 1:
 			raise ValueError("At least one simulation should be specified")
 		
+		self._kwargs = kwargs
 		self.simdirs = simdirs
-		self.realizations = []
 		
 		import joblib #Keep this import here so that the rest of the functions are usable without joblib installed.
+		cachedir = kwargs.pop('cachedir', None)
 		memory = joblib.Memory(cachedir)
-		disp_rel = memory.cache(
-			disp_rel,
+		self._dr_type = memory.cache(
+			dr_type,
 			cache_validation_callback = self._cache_validation_callback
 			)
 		
-		for simdir in self.simdirs:
-			dr = disp_rel(simdir=simdir, **kwargs)
+		self.realizations = []
+		for simdir in simdirs:
+			dr = self._dr_type(simdir=simdir, **kwargs)
 			
 			#These warnings are here again to cover the case when the realizations were loaded from the cache.
 			if dr.t_min < dr.ts.t[0]:
@@ -270,17 +280,80 @@ class drs_holder():
 			
 			self.realizations.append(dr)
 		
+		dr = self.realizations[0]
 		for attr in [
 			"param",
 			"dim",
 			"grid",
+			"k_tilde_min",
+			"k_tilde_max",
+			"omega_tilde_min",
+			"omega_tilde_max",
+			"fig_savedir",
+			"field_name",
+			"cbar_label",
 			"ts",
 			"av_xy",
-			"kx_tilde",
-			"cbar_label",
 			]:
-			if hasattr(self.realizations[0], attr):
-				setattr(self, attr, getattr(self.realizations[0], attr))
+			if hasattr(dr, attr):
+				setattr(self, attr, getattr(dr, attr))
+		
+		self.set_t_range(self.realizations[0].t_min, self.realizations[0].t_max)
+	
+	@property
+	def z(self):
+		return self.realizations[0].z
+	
+	def do_ft(self):
+		n_intervals = len(self.realizations)
+		
+		data_mean = 0
+		data2_sum = 0
+		for dr in self.realizations:
+			dr.set_t_range(self.t_min, self.t_max)
+			
+			data_mean += dr.data
+			data2_sum += dr.data**2
+			
+		data_mean = data_mean/n_intervals
+		if n_intervals == 1:
+			sigma = np.full_like(data_mean, np.nan)
+		else:
+			#sqrt(n/(n-1)) is to reduce the bias in the estimate for the standard deviation.
+			sigma = np.sqrt(data2_sum/n_intervals - (data_mean)**2)*np.sqrt(n_intervals/(n_intervals-1))
+		
+		self.omega = dr.omega
+		self.data = data_mean
+		self.sigma = sigma/np.sqrt(n_intervals)
+		
+		if hasattr(dr, "kx"):
+			self.kx = dr.kx
+		if hasattr(dr, "ky"):
+			self.ky = dr.ky
+	
+	def __getnewargs_ex__(self):
+		"""
+		Make this class pickle-able
+		"""
+		return ((self._dr_type, self.simdirs), self._kwargs)
+	
+	def __getattr__(self, name):
+		if name in self._kwargs.keys():
+			return self._kwargs[name]
+		else:
+			"""
+			Most examples I've seen just raise AttributeError, but the below seems to be needed to correctly inherit properties from superclasses.
+			"""
+			return self.__getattribute__(name)
+	
+	def __setattr__(self, name, value):
+		if name == "_kwargs":
+			self.__dict__[name] = value
+		elif name in self._kwargs.keys():
+			self._kwargs[name] = value
+		else:
+			# https://stackoverflow.com/questions/7042152/how-do-i-properly-override-setattr-and-getattribute-on-new-style-classes/7042247#7042247
+			super().__setattr__(name, value)
 	
 	def _cache_validation_callback(self, metadata):
 		"""
