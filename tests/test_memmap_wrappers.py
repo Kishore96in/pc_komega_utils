16878b2f7aec7af5a5128f8e8143c356d4f220a1
Kishore Gopalakrishnan <kishore96@gmail.com>
2024-04-19 11:55:20 +0530

    test_mmap_wrappers: add another test

--- tests/test_memmap_wrappers.py
+++ tests/test_memmap_wrappers.py
@@ -10,3 +10,9 @@ def test_create_delete():
 	tmpfilename = a.tmpfilename
 	del a
 	assert not os.path.exists(tmpfilename)
+
+def test_create_2():
+	a = mmap_array("/tmp", shape=(3,4,5), dtype=int)
+	
+	assert a.shape == (3,4,5)
+	assert a.dtype == int
