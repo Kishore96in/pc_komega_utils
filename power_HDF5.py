dec81558ef528eaf96c4b55d39c82edd9d2cc36f
Kishore Gopalakrishnan <kishore96@gmail.com>
2024-04-20 17:32:11 +0530

    read_power_cached: first attempt at a HDF5-backed object to store the Power data
    Not tested

---
+++ power_HDF5.py
@@ -0,0 +1,33 @@
+"""
+Replacement for pc.read.powers.Power that stores the data in a HDF5 file, rather than in memory.
+"""
+
+import pencil as pc
+import h5py
+import os
+
+class read_power_cached():
+	def __init__(self, *args, cachedir=".", **kwargs):
+		fname = os.path.join(cachedir, "power_cache.h5")
+		self.cache = h5py.File(fname, 'w')
+		
+		p = pc.read.power(*args, **kwargs)
+		for k in p.keys():
+			self._h5cache(k, getattr(p,k))
+	
+	def _h5cache(self, name, value):
+		"""
+		name : str
+		value : numpy array
+		"""
+		dset = self.cache.create_dataset(name, value.shape, dtype=value.dtype)
+		dset[()] = value
+	
+	def __getattr__(self, name):
+		if name in self.cache.keys():
+			return self.cache[name]
+		else:
+			raise AttributeError
+	
+	def __del__(self):
+		self.cache.close()
