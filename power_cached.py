b9d5255decedf6d93b6e2a155ca8ae9d8f081706
Kishore Gopalakrishnan <kishore96@gmail.com>
2024-04-22 16:42:58 +0530

    read_power_cached: now allow math operations on the cached dataset
    Earlier, it would complain about _add__ etc. not being defined for h5py
    datasets.

--- power_cached.py
+++ power_cached.py
@@ -5,6 +5,7 @@ Replacement for pc.read.powers.Power that stores the data in a HDF5 file, rather
 import pencil as pc
 import h5py
 import os
+import numpy as np
 
 class read_power():
 	"""
@@ -48,7 +49,7 @@ class read_power():
 			if data.ndim == 0:
 				return data[()]
 			else:
-				return data
+				return h5py_dataset_wrapper(data)
 		else:
 			raise AttributeError
 	
@@ -63,3 +64,62 @@ class read_power():
 	
 	def __setstate__(self, cachename):
 		self._cache = h5py.File(cachename, 'r')
+
+class h5py_dataset_wrapper():
+	"""
+	Allows mathematical operations to be performed on h5py Dataset instances as if they are numpy arrays.
+	"""
+	def __init__(self, dset):
+		self._dset = dset
+	
+	def __getattr__(self, name):
+		return getattr(self._dset, name)
+	
+	def __getitem__(self, key):
+		return self._dset[key]
+	
+	def __len__(self):
+		return len(self._dset)
+	
+	# List of magic methods: https://python-course.eu/oop/magic-methods.php
+	# Binary operators
+	def __add__(self, other):
+		return np.add(self, other)
+	def __radd__(self, other):
+		return np.add(other, self)
+	def __sub__(self, other):
+		return np.subtract(self, other)
+	def __rsub__(self, other):
+		return np.subtract(other, self)
+	def __mul__(self, other):
+		return np.multiply(self, other)
+	def __rmul__(self, other):
+		return np.multiply(other, self)
+	def __floordiv__(self, other):
+		return np.floor_divide(self, other)
+	def __truediv__(self, other):
+		return np.true_divide(self, other)
+	def __mod__(self, other):
+		return np.mod(self, other)
+	def __pow__(self, other):
+		return np.power(self, other)
+	
+	# Unary operators
+	def __neg__(self):
+		return -1*self
+	def __abs__(self):
+		return np.abs(self)
+	
+	# Comparison operators
+	def __lt__(self, other):
+		return np.less(self, other)
+	def __le__(self, other):
+		return np.less_equal(self, other)
+	def __eq__(self, other):
+		return np.equal(self, other)
+	def __ne__(self, other):
+		return np.not_equal(self, other)
+	def __ge__(self, other):
+		return np.greater_equal(self, other)
+	def __gt__(self, other):
+		return np.greater(self, other)
