63c2159ba4b775193daecb75e5c8750bf45892c4
Kishore Gopalakrishnan <kishore96@gmail.com>
2024-05-31 15:20:12 +0530

    fit.py: _default_getter: update behaviour
    To match what get_mode_eigenfunction now expects

--- fit.py
+++ fit.py
@@ -66,7 +66,11 @@ def _default_getter(dr, k_tilde, z, om_tilde_min, om_tilde_max):
 		z = z,
 		compress = True,
 		)
-	return data_near_target, omt_near_target
+	return {
+		'data_near_target' = data_near_target,
+		'omt_near_target' = omt_near_target,
+		'sigma': None,
+		}
 
 def fit_mode(
 	data_near_target,
