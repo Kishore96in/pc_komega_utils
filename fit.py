ed04e4907d17a63c5f52f5ecb029ec36a97b859e
Kishore Gopalakrishnan <kishore96@gmail.com>
2024-07-18 12:08:24 +0530

    fit._get_mode_mass_err_mc: fix error message
    missing = for ulim

--- fit.py
+++ fit.py
@@ -624,6 +624,6 @@ def _get_mode_mass_err_mc(popt, perr, **kwargs):
 	err_minus = mass - llim
 	
 	if not (np.all(err_plus > 0) and np.all(err_minus > 0)):
-		raise RuntimeError(f"Non-positive errors:\n\t{err_minus = }\n\t{err_plus = }\n\t{llim = }\n\t{ulim}\n\t{mass = }")
+		raise RuntimeError(f"Non-positive errors:\n\t{err_minus = }\n\t{err_plus = }\n\t{llim = }\n\t{ulim = }\n\t{mass = }")
 	
 	return err_minus, err_plus
