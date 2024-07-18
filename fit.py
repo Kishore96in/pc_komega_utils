83162c467a75d0b26db79168a6224e652c0ee41b
Kishore Gopalakrishnan <kishore96@gmail.com>
2024-07-18 12:03:50 +0530

    fit._get_mode_mass_err_mc: fix a typo

--- fit.py
+++ fit.py
@@ -621,7 +621,7 @@ def _get_mode_mass_err_mc(popt, perr, **kwargs):
 	mass = _get_mode_mass(popt = popt, **kwargs)
 	
 	err_plus = ulim - mass
-	err_minus = popt - mass
+	err_minus = mass - llim
 	
 	if not (np.all(err_plus > 0) and np.all(err_minus > 0)):
 		raise RuntimeError(f"Non-positive errors:\n\t{err_minus = }\n\t{err_plus = }")
