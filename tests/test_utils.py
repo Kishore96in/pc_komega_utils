b0d941adcf1b87fca728f200eb74af72104133fa
Kishore Gopalakrishnan <kishore96@gmail.com>
2024-07-21 10:22:11 +0530

    Add another test for smooth_gauss

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -21,3 +21,20 @@ def test_smooth_gauss_multidim():
 	
 	sm_3ds = smooth_gauss(np.swapaxes(data_3d,1,2), 1, axis=1)
 	assert np.all(sm_1d == sm_3ds[0,:,0])
+
+def test_smooth_gauss_multidim2():
+	"""
+	Check handling of multidimensional arrays in smooth_gauss.
+	"""
+	data1 = np.random.default_rng(seed=42).uniform(size=10)
+	data2 = np.random.default_rng(seed=42).uniform(size=10)
+	
+	sm1_1d = smooth_gauss(data1, 1)
+	sm2_1d = smooth_gauss(data2, 1)
+	
+	data_2d = np.stack([data1, data2])
+	
+	sm_2d = smooth_gauss(data_2d, 1, axis=1)
+	assert np.all(sm1_1d == sm_2d[0,:])
+	assert np.all(sm2_1d == sm_2d[1,:])
+
