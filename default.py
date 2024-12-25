cb6919c71f97de544a928f70a2d059079a3c101c
Kishore Gopalakrishnan <kishore96@gmail.com>
2024-12-25 16:55:44 +0530

    default: change scaling mixin

--- default.py
+++ default.py
@@ -4,11 +4,20 @@ Only meant for quick interactive use; may change without notice.
 
 from . import read
 
-class dr_yaver(read.m_dscl_rdbyurmsmax, read.m_scl_HP, read.dr_yaver_base):
-	pass
+class dr_yaver(
+	read.m_dscl_d2,
+	read.m_scl_HP,
+	read.dr_yaver_base,
+	): pass
 
-class dr_dvar(read.m_dscl_rdbyurmsmax, read.m_scl_HP, read.dr_dvar_base):
-	pass
+class dr_dvar(
+	read.m_dscl_d2,
+	read.m_scl_HP,
+	read.dr_dvar_base,
+	): pass
 
-class dr_pxy(read.m_dscl_rdbyurmsmax, read.m_scl_HP, read.dr_pxy_base):
-	pass
+class dr_pxy(
+	read.m_dscl_d2,
+	read.m_scl_HP,
+	read.dr_pxy_base,
+	): pass
