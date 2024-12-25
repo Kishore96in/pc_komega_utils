9415432967b9fcb6a9f65b0cf1fc9bb31b983210
Kishore Gopalakrishnan <kishore96@gmail.com>
2024-12-25 16:56:14 +0530

    default: add mixin that reads power_xy from HDF5 files

--- default.py
+++ default.py
@@ -3,6 +3,7 @@ Only meant for quick interactive use; may change without notice.
 """
 
 from . import read
+from . import power
 
 class dr_yaver(
 	read.m_dscl_d2,
@@ -19,5 +20,12 @@ class dr_dvar(
 class dr_pxy(
 	read.m_dscl_d2,
 	read.m_scl_HP,
+	read.dr_pxy_base,
+	): pass
+
+class dr_pxy_h5(
+	read.m_dscl_d2,
+	read.m_scl_HP,
+	power.dat2h5.m_pxy_h5,
 	read.dr_pxy_base,
 	): pass
