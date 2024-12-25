0d644bab7566740da11ebe8c9bb69bdf5d661e26
Kishore Gopalakrishnan <kishore96@gmail.com>
2024-12-25 12:36:50 +0530

    test_power_dat2hdf: add more tests

--- tests/test_power_dat2hdf.py
+++ tests/test_power_dat2hdf.py
@@ -43,3 +43,16 @@ def test_powerxy_to_hdf5():
 	
 	with h5py.File(out_file) as f:
 		assert f['nzpos'][()] == p_src.nzpos
+		assert len(f['kx']) == 101
+		assert len(f['ky']) == 1
+		assert len(f['times/it']) == 2
+		assert len(f['times/t']) == 2
+		
+		assert "uz_xy" in f.keys()
+		assert len(f['uz_xy'].keys()) == len(f['times/it'])
+		
+		it = f['times/it'][0]
+		assert np.all(p_src.uz_xy[0] == f['uz_xy'][str(it)])
+		
+		it = f['times/it'][1]
+		assert np.all(p_src.uz_xy[1] == f['uz_xy'][str(it)])
